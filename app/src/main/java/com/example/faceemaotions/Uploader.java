e746e97777e61bd395c3911e1e6e419eb0978536
anieo <ali.maher.1460@gmail.com>
2019-02-12 20:45:33 +0200

    Uploader Base

--- app/src/main/java/com/example/faceemaotions/Uploader.java
+++ app/src/main/java/com/example/faceemaotions/Uploader.java
@@ -1,85 +1,79 @@
 package com.example.faceemaotions;
 
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.util.Base64;
-import android.util.Log;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public class Uploader
 {
-    String imgString;
-    String surl;
-    String oImgString;
+    private static String BASE_URL="";
+    private static Retrofit retrofit;
+    private         Context context;
 
-
-
-    public Uploader(Bitmap bitmap, String urlString ) {
-
-        //The Image is now turned into byte array and encoded into 64 base string ready to be sent
-        imgString= Base64.encodeToString(getByteArrayFromBitmap(bitmap),Base64.NO_WRAP);
-        surl=urlString;
-        try{
-
-        }catch(Exception ex){
-            Log.i("URL FORMATION","MALFORMATED URL");
-        }
+    Uploader(Context context,String url){
+        this.context=context;
+        this.BASE_URL=url;
 
     }
-    private void httpSender (){
-        OutputStream out=null;
-        try {
-            URL url=new URL(surl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(15000);
-            conn.setConnectTimeout(15000);
-            conn.setRequestMethod("POST");
-            conn.setDoInput(true);
-            conn.setDoOutput(true);
-            out = new BufferedOutputStream(conn.getOutputStream());
+    public static Retrofit getRetrofit(Context context){
+        if(retrofit == null){
+            OkHttpClient okHttpClient=new OkHttpClient.Builder().build();
+            retrofit=new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit;
+    }
 
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out,"UTF-8"));
-            writer.write(imgString);
-            writer.flush();
-            writer.close();
-            out.close();
+    public interface UploadAPIs {
+        @Multipart
+        @POST("/upload")
+        Call uploadImage(@Part MultipartBody.Part file, @Part("name") RequestBody requestBody);
+    }
 
-            conn.connect();
-            int response = conn.getResponseCode();
-            if( response== HttpURLConnection.HTTP_OK){
-                String line;
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                while((line=bufferedReader.readLine()) !=null)
-                {
-                    oImgString+=line;
-                }
+
+    private void uploadToServer(String filePath) {
+        Retrofit retrofit = Uploader.getRetrofit(context);
+        UploadAPIs uploadAPIs = retrofit.create(UploadAPIs.class);
+        //Create a file object using file path
+        File file = new File(filePath);
+        // Create a request body with file and image media type
+        RequestBody fileReqBody = RequestBody.create(MediaType.parse("image/*"), file);
+        // Create MultipartBody.Part using file request-body,file name and part name
+        MultipartBody.Part part = MultipartBody.Part.createFormData("upload", file.getName(), fileReqBody);
+        //Create request body with text description and text media type
+        RequestBody description = RequestBody.create(MediaType.parse("text/plain"), "image-type");
+        //
+        Call call = uploadAPIs.uploadImage(part, description);
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+                System.out.println(response);
             }
 
-        }catch(Exception ex){
-            System.out.println(ex.getMessage());
-        }
-    }
-    private byte[] getByteArrayFromBitmap(Bitmap bitmap)
-    {
-        ByteArrayOutputStream stream=new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
-        return stream.toByteArray();
-    }
-
-    private Bitmap getBitmapFromByteArray(byte[] byteImage)
-    {
-        return BitmapFactory.decodeByteArray(byteImage,0,byteImage.length);
+            @Override
+            public void onFailure(Call call, IOException t) {
+            }
+        });
     }
 
 
