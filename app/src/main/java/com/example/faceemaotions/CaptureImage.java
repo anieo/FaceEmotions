3fecbdc0f51a871aa5b00cffb1ac86cb3cfef09e
anieo <ali.maher.1460@gmail.com>
2019-02-12 20:45:33 +0200

    Uploader Base

--- app/src/main/java/com/example/faceemaotions/CaptureImage.java
+++ app/src/main/java/com/example/faceemaotions/CaptureImage.java
@@ -6,11 +6,7 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -18,6 +14,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 
 public class CaptureImage  {
 
